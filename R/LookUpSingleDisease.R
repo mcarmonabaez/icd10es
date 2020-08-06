4b631d55e3a17bde321ffcbafdedf1633f642071
Mariana Carmona <mcarmonabaez@MacBook-Air-de-Mariana.local>
2020-08-05 21:21:20 -0400

    continue with diagram

--- R/LookUpSingleDisease.R
+++ R/LookUpSingleDisease.R
@@ -21,9 +21,20 @@ ICDLookUp <- function(pattern, nMatches = 1){
     return(matchICD)
   }
 
-  #Look up exact match in category catalog
-  str_match(categories$padecimiento, pattern)
+  #Look up exact match in disease catalog
+  diseaseToPattern <- str_match(pattern, categories$padecimiento)
+  patternToDisease <- str_match(categories$padecimiento, pattern)
+  flagMatch <- sum(diseaseToPattern == patternToDisease, na.rm = T)
 
+  if(flagMatch == 1) {
+    #devolver subcategoria y clave y si es principal o alternativo
+  }
+
+  #Look up exact match in category catalog
+  if(flagMatch == 0) {
+    #buscar en el catalogo de categorias
+    # Hay categoria entonces regresamos el X009, si tiene subcategorias, sino regresar categoria (A09)
+  }
 
 }
 
